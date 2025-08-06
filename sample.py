1e175afb03e44d34be5fc622f995487ec17f15cf
reauso <r.ebertowski@gmx.de>
2025-08-06 14:45:41 +0200

    changed pyproject.toml to include pixi and to use better dependency versions

--- sample.py
+++ sample.py
@@ -4,18 +4,31 @@ from zonos.model import Zonos
 from zonos.conditioning import make_cond_dict
 from zonos.utils import DEFAULT_DEVICE as device
 
+print('Load Model')
 # model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-hybrid", device=device)
 model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-transformer", device=device)
 
-wav, sampling_rate = torchaudio.load("assets/exampleaudio.mp3")
+print('Load Sample Audio file and make embedding')
+# wav, sampling_rate = torchaudio.load("assets/exampleaudio.mp3")
+wav, sampling_rate = torchaudio.load("assets/Rene_120s_DE.mp3")
 speaker = model.make_speaker_embedding(wav, sampling_rate)
 
 torch.manual_seed(421)
 
-cond_dict = make_cond_dict(text="Hello, world!", speaker=speaker, language="en-us")
+print('Prepare')
+# cond_dict = make_cond_dict(text="Hi my name is Rene and my favourite color is blue.", speaker=speaker, language="de-de")
+cond_dict = make_cond_dict(
+    text="Hallo, mein Name ist Rene und meine Lieblingsfarbe ist Blau.",
+    speaker=speaker,
+    language="de",
+)
 conditioning = model.prepare_conditioning(cond_dict)
 
+print('Generate Code')
 codes = model.generate(conditioning)
 
+print('Create wav')
 wavs = model.autoencoder.decode(codes).cpu()
-torchaudio.save("sample.wav", wavs[0], model.autoencoder.sampling_rate)
+
+print('Export Wav')
+torchaudio.save("sample_de.wav", wavs[0], model.autoencoder.sampling_rate)
